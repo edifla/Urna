2bb1f99f49f377f0594634ea558f24a338345c49
edifla <rafa.cosra2015@gmail.com>
2020-10-12 19:00:38 -0300

    first Commit

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        titulo :"VEREADOR",
+        numero: 5,
+        canditados: [//Inicia o array de candidatos
+            {//Candidato 1                
+                numero:'38111',
+                nome:"Fulano debal",
+                partido: "Abc",
+                fotos:[
+                    {url:'do-utilizador.svg', legenda:'Vereador', small: false}
+                ]
+            },
+            {//Candidato 2
+                numero:'00111',
+                nome:"Beltrano debal",
+                partido: "BCD",
+                fotos:[
+                    {url:'do-utilizador.svg', legenda:'Vereador'}
+                ]
+            },
+        ]//Finaliza o array de candidatos
+
+    },
+    {
+        titulo :"PREFEITO",
+        numero: 2,
+        canditados: [//Inicia o array de candidatos
+            {//Candidato 1                
+                numero:'99',
+                nome:"Clano",
+                partido: "Abc",
+                vice: "cic",
+                fotos:[
+                    {url:'do-utilizador.svg', legenda:'Prefeito'},
+                    {url:'do-utilizador.svg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {//Candidato 2
+                numero: '88',
+                nome:"belano",
+                partido: "Abc",
+                vice: "Zul",
+                fotos:[
+                    {url:'do-utilizador.svg', legenda:'Prefeito'},
+                    {url:'do-utilizador.svg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+        ]//Finaliza o array de candidatos
+    }
+
+
+]
